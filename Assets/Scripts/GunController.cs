e300cf90b2b3dc0058fb9bd9d8c1d98c74f613a8
thwig <tyler.hwig@gmail.com>
2025-07-19 22:21:12 -0500

    Create player sprite

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,6 +9,7 @@ public class GunController : MonoBehaviour
     [SerializeField] private float firingRate;
     [SerializeField] private float delayBeforeFiring;
     [SerializeField] private float damage;
+    [SerializeField] private float bulletOffset;
     private float nextFiringTime = 0f;
     private bool buffering = false;
 
@@ -29,7 +30,8 @@ public class GunController : MonoBehaviour
         Vector2 finalDir = new Vector2(Input.GetAxisRaw("HorizontalFire"), Input.GetAxisRaw("VerticalFire")).normalized;
         if (finalDir != Vector2.zero && Time.time >= nextFiringTime)
         {
-            BulletController bullet = bulletPool.GetBullet(transform.position, BulletRotation()).GetComponent<BulletController>();
+            Vector2 bulletPos = new Vector2(transform.position.x, transform.position.y + bulletOffset);
+            BulletController bullet = bulletPool.GetBullet(bulletPos, BulletRotation()).GetComponent<BulletController>();
             bullet.SetVelocity(finalDir, velocity);
             bullet.SetDamage(damage);
             nextFiringTime = Time.time + (1f / firingRate);
